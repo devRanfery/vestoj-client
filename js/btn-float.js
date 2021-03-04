5716dbb7ade8f58fbcb3c1307a1ed00dfbb5fac8
Ranfery Alvarez <devRanfery.github.com>
2021-03-04 02:42:58 -0600

    fists commit

---
+++ js/btn-float.js
@@ -0,0 +1,51 @@
+// $(document).ready(function () {
+//   $('#hide').on('click', function () {
+//     $('#element').hide();
+//     return false;
+//   });
+
+//   $('#show').on('click', function () {
+//     $('#element').show();
+//     return false;
+//   });
+// });
+$(document).ready(function () {
+  $('#min').click(function () {
+    //Solo si el valor del campo es diferente de 0
+    if ($('#cantidadInput').val() != 0)
+      //Decrementamos su valor
+      $('#cantidadInput').val(parseInt($('#cantidadInput').val()) - 1);
+  });
+
+  $('#plus').click(function () {
+    //Aumentamos el valor del campo
+    $('#cantidadInput').val(parseInt($('#cantidadInput').val()) + 1);
+  });
+});
+
+// BTN TOGGLE SOCIAL
+$('.container-social').hide();
+$('#btn-dialog-social-ocultar').hide();
+
+$('#btn-dialog-social').click(function () {
+  $('.container-social').show();
+  $('#btn-dialog-social').hide();
+  $('#btn-dialog-social-ocultar').show();
+});
+
+$('#btn-dialog-social-ocultar').click(function () {
+  $('.container-social').hide();
+  $('#btn-dialog-social').show();
+  $('#btn-dialog-social-ocultar').hide();
+});
+
+$('#viewRegister').click(function () {
+  $('#loginModal').modal('hide');
+  $('#modalRegister').modal('show');
+});
+
+$('#recuperar').click(function () {
+  $('#loginModal').modal('hide');
+  $('#modalRegister').modal('hide');
+  $('#passwordModal').modal('show');
+});
